64ca25ceeb97418794a9fbf152cd7d145145524f
siss-ankisile <ankisile@gmail.com>
2023-01-01 23:09:40 +0900

    프로젝트 생성

---
+++ src/main/java/com/oasis/graduationspringbootappservice/GraduationSpringbootAppserviceApplication.java
@@ -0,0 +1,13 @@
+package com.oasis.graduationspringbootappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationSpringbootAppserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationSpringbootAppserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oasis/graduationspringbootappservice/GraduationSpringbootAppserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.graduationspringbootappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationSpringbootAppserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

e8d8f7b12f641537070f0e79bb4a1b216f88cfb6
siss-ankisile <ankisile@gmail.com>
2023-05-26 23:06:14 +0900

    code: WeatherController return 형식 변경

--- src/main/java/com/oasis/springboot/controller/WeatherController.java
+++ src/main/java/com/oasis/springboot/controller/WeatherController.java
@@ -1,5 +1,9 @@
 package com.oasis.springboot.controller;
 
+import com.oasis.springboot.common.response.ListResponse;
+import com.oasis.springboot.common.response.ResponseService;
+import com.oasis.springboot.dto.weather.WeatherApiResponseDTO;
+import com.oasis.springboot.dto.weather.WeatherDTO;
 import com.oasis.springboot.service.WeatherService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -7,32 +11,25 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @Tag(name = "home", description = "메인화면 API")
 @RestController
 @RequestMapping("/api/home")
 @RequiredArgsConstructor
-public class HomeController {
+public class WeatherController {
 
     private final WeatherService weatherService;
+    private final ResponseService responseService;
 
     @Operation(summary = "get weather", description = "지역에 대한 날씨를 가져오기")
     @Parameters({
@@ -40,8 +37,8 @@ public class HomeController {
             @Parameter(name = "y", description = "y좌표", example = "127"),
     })
     @GetMapping("")
-    public JSONArray getWeather(@RequestParam("x") String x, @RequestParam("y") String y) throws IOException, ParseException {
-        return weatherService.getWeather(x, y);
+    public ListResponse<WeatherDTO> getWeather(@RequestParam("x") String x, @RequestParam("y") String y) throws IOException, ParseException, URISyntaxException {
+        return responseService.getListResponse(weatherService.getWeather(x, y));
     }
 
 }

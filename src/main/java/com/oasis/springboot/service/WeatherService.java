0fdce53c28edc75fbab311854ea9109f3fa46e0a
siss-ankisile <ankisile@gmail.com>
2023-05-26 23:11:49 +0900

    refactor: WeatherService 코드 리팩토링 완료 - 함수 쪼개기 - 변수명 변경 - 리턴형식 변경

--- src/main/java/com/oasis/springboot/service/WeatherService.java
+++ src/main/java/com/oasis/springboot/service/WeatherService.java
@@ -1,41 +1,63 @@
 package com.oasis.springboot.service;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.oasis.springboot.dto.weather.WeatherApiResponseDTO;
+import com.oasis.springboot.dto.weather.WeatherDTO;
+import com.oasis.springboot.dto.weather.WeatherItemDTO;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WeatherService {
 
-    public JSONArray getWeather(String x, String y) throws IOException, ParseException, ParseException {
-        String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst";
+    @Value("${weather.api.key}")
+    private String serviceKey;
 
-        String serviceKey = "UdJoe0RU%2BfA4noQhiQupw5F43s9H%2BGaTyKVFX9guM4wsuoWR9SWXPtQ4pHzWpcOpP%2FBo6enHnmibSfN55xy7tQ%3D%3D";
+    public List<WeatherDTO> getWeather(String x, String y) throws IOException, ParseException, URISyntaxException {
+
+        URI uri = makeRequestURI(x, y);
+
+        RestTemplate template = new RestTemplate();
+        HttpEntity<String> entity = new HttpEntity<>(this.getHeaders());
+
+        ResponseEntity<WeatherApiResponseDTO> responseDto = template.exchange(uri, HttpMethod.GET, entity, WeatherApiResponseDTO.class);
+
+        List<WeatherItemDTO> itemList = responseDto.getBody().getResponse().getBody().getItems().getItem();
+
+        List<WeatherDTO> weatherDTOList = new ArrayList<>();
+        for(int i=0;i<10;i++) {
+            String category = itemList.get(i*6).getCategory();
+            String value = itemList.get(i*6).getFcstValue();
+
+            WeatherDTO weatherDTO = new WeatherDTO(category, value);
+            weatherDTOList.add(weatherDTO);
+        }
+
+        return weatherDTOList;
+    }
+
+    private URI makeRequestURI(String x, String y) throws UnsupportedEncodingException, URISyntaxException {
+        String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst";
 
         String nx = x;
         String ny = y;
-        String baseDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        String baseTime = String.format("%02d", LocalTime.now().getHour())+"30";
-        if(LocalTime.now().getMinute()<30) {
-            baseTime =  String.format("%02d", LocalTime.now().getHour() - 1) + "30";
-            if(LocalTime.now().getHour() == 0) {
-                baseDate = LocalDateTime.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-                baseTime = "2330";
-            }
-        }
-        String dataType = "json";
+        String baseDate = setBaseDate();
+        String baseTime = setBaseTime();
+        String dataType = "JSON";
         String numOfRows = "250";
 
         StringBuilder urlBuilder = new StringBuilder(apiUrl);
@@ -47,55 +69,38 @@ public class WeatherService {
         urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(dataType, "UTF-8"));	/* 타입 */
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(numOfRows, "UTF-8"));	/* 한 페이지 결과 수 */
 
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
+        return new URI(urlBuilder.toString());
 
-        BufferedReader rd;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+    }
+
+
+    private String setBaseDate(){
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        String baseDate = currentDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        if(currentDateTime.getMinute()<30) {
+            if(currentDateTime.getHour() == 0) {
+                baseDate = LocalDateTime.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+            }
         }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
+        return baseDate;
+    }
+
+    private String setBaseTime(){
+        LocalTime currentTime = LocalTime.now();
+        String baseTime = String.format("%02d", currentTime.getHour())+"30";
+        if(currentTime.getMinute()<30) {
+            baseTime =  String.format("%02d", currentTime.getHour() - 1) + "30";
+            if(currentTime.getHour() == 0) {
+                baseTime = "2330";
+            }
         }
-        rd.close();
-        conn.disconnect();
-        String data= sb.toString();
+        return baseTime;
+    }
 
-        // Json parser를 만들어 만들어진 문자열 데이터를 객체화
-        JSONParser parser = new JSONParser();
-        JSONObject obj = (JSONObject) parser.parse(data);
-        // response 키를 가지고 데이터를 파싱
-        JSONObject parse_response = (JSONObject) obj.get("response");
-        // response 로 부터 body 찾기
-        JSONObject parse_body = (JSONObject) parse_response.get("body");
-        // body 로 부터 items 찾기
-        JSONObject parse_items = (JSONObject) parse_body.get("items");
-        JSONArray parse_item = (JSONArray) parse_items.get("item");
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "JSON", Charset.forName("UTF-8")));
 
-
-        JSONArray result = new JSONArray();
-        Object fcstTime;
-        Object category;
-        Object value;
-
-        for(int k=0;k<10;k++) {
-            JSONObject weather = new JSONObject();
-            weather = (JSONObject) parse_item.get(k*6);
-            JSONObject object = new JSONObject();
-            category = weather.get("category");
-            value = weather.get("fcstValue");
-
-            object.put("Category", category);
-            object.put("Value", value);
-            result.add(object);
-        }
-
-        return result;
+        return headers;
     }
 }

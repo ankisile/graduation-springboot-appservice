c9ee03c1b8e052b43ad5ff4e19a77ae6f6ebc4f6
siss-ankisile <ankisile@gmail.com>
2023-05-30 00:03:53 +0900

    test: UserTemplate 생성 -UserService 테스트 하기위한 template 생성

--- src/test/java/com/oasis/springboot/user/UserTemplate.java
+++ src/test/java/com/oasis/springboot/user/UserTemplate.java
@@ -1,8 +1,9 @@
-package com.oasis.springboot.template;
+package com.oasis.springboot.user;
 
 import com.oasis.springboot.domain.user.Role;
 import com.oasis.springboot.domain.user.User;
-import com.oasis.springboot.dto.SignUpRequestDto;
+import com.oasis.springboot.dto.user.SignUpRequestDto;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.io.FileInputStream;
@@ -18,6 +19,10 @@ public class UserTemplate {
         return new SignUpRequestDto(EMAIL, PASSWORD, NICKNAME, null);
     }
 
+    public static SignUpRequestDto makeTestSingUpRequestDtoWithImage() throws Exception{
+        return new SignUpRequestDto(EMAIL, PASSWORD, NICKNAME, getProfileImage());
+    }
+
     public static User makeTestUser() throws Exception {
         return User.builder()
                 .email(EMAIL)
@@ -27,4 +32,11 @@ public class UserTemplate {
                 .role(Role.USER).build();
     }
 
+    public static MockMultipartFile getProfileImage() throws Exception {
+        String fileName = "testImage";
+        String contentType = "jpg";
+        String filePath = "src/test/resources/images/" + fileName + "." + contentType;
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        return new MockMultipartFile("profileImage", fileName + "." + contentType, contentType, fileInputStream);
+    }
 }

5ec6645ffa4646a70653d7b022de940a6fa88e69
siss-ankisile <ankisile@gmail.com>
2023-05-29 23:53:09 +0900

    doc: UserController 문서 변경

--- src/main/java/com/oasis/springboot/controller/UserController.java
+++ src/main/java/com/oasis/springboot/controller/UserController.java
@@ -7,12 +7,12 @@ import com.oasis.springboot.common.exception.NotMatchPasswordException;
 import com.oasis.springboot.common.response.CommonResponse;
 import com.oasis.springboot.common.response.ResponseService;
 import com.oasis.springboot.common.response.SingleResponse;
-import com.oasis.springboot.dto.PasswordDto;
-import com.oasis.springboot.dto.SignUpRequestDto;
-import com.oasis.springboot.dto.UserMainResponseDto;
+import com.oasis.springboot.dto.user.PasswordDto;
+import com.oasis.springboot.dto.user.SignUpRequestDto;
+import com.oasis.springboot.dto.user.UpdateUserRequestDto;
+import com.oasis.springboot.dto.user.UserMainResponseDto;
 import com.oasis.springboot.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -21,14 +21,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 
 
-@Tag(name = "유저 회원가입", description = "")
+@Tag(name = "유저", description = "")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -40,7 +38,8 @@ public class UserController {
     @Operation(summary = "회원가입", description = "form-data 형식으로 전달 필요")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "successful operation"),
-            @ApiResponse(responseCode = "1002", description = "Exist User", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
+            @ApiResponse(responseCode = "1002", description = "Exist User", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(responseCode = "1006", description = "File Upload Fail", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
     })
     @PostMapping(value = "/signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public SingleResponse<Long> signup(@ModelAttribute @Valid SignUpRequestDto signUpRequestDto) {
@@ -53,19 +52,14 @@ public class UserController {
         return responseService.getSingleResponse(userService.findUserInfo());
     }
 
-    @Operation(security = { @SecurityRequirement(name = "bearer-key") }, summary = "정보 수정", description = "form-data 형식으로 전달 필요")
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") }, summary = "정보 수정", description = "form-data 형식으로 전달 필요. 사진 변경되지 않으면 null 기본이미지로 바뀔때는 boolean 값만 전달 사진은 null")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "1006", description = "File Upload Fail", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
+    })
     @PatchMapping(value ="/user/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public SingleResponse<String> updateUserInfo(
-            @Schema(description = "유저정보 닉네임만(String)",
-                    example = "nickName"
-            )
-            @RequestPart(value = "name", required = false) String nickName,
-            @Schema(description = "사진 변경 여부. boolean인데 string으로 전달 바람 true false 이런식으로")
-            @RequestPart(value = "change") String isChange,
-            @Parameter(name = "file", description = "사진(maxSize: 10MB), 변경되지 않으면 null 기본이미지로 바뀔때는 boolean 값만 전달 사진은 null")
-            @RequestPart(value = "file", required = false) MultipartFile file
-    ){
-        return responseService.getSingleResponse(userService.updateUserInfo(nickName, Boolean.parseBoolean(isChange), file));
+    public SingleResponse<String> updateUserInfo(@ModelAttribute UpdateUserRequestDto updateUserRequestDto){
+        return responseService.getSingleResponse(userService.updateUserInfo(updateUserRequestDto));
     }
 
     @Operation(security = { @SecurityRequirement(name = "bearer-key") }, summary = "비밀번호 변경")

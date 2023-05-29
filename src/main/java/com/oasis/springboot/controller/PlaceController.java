130bf6fc6e66e8048b95d259d922f8de47a28f6b
siss-ankisile <ankisile@gmail.com>
2023-05-30 00:11:34 +0900

    doc: PlaceController 문서 변경

--- src/main/java/com/oasis/springboot/controller/PlaceController.java
+++ src/main/java/com/oasis/springboot/controller/PlaceController.java
@@ -5,12 +5,14 @@ import com.oasis.springboot.service.PlaceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "식물샵 위치")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")

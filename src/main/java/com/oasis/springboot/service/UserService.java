70a4178d8ce2f290a91d3e8bf59e1b30c8d20cbd
siss-ankisile <ankisile@gmail.com>
2023-06-21 01:51:09 +0900

    code: UserService 코드 변경 - signup, updateUserInfo 함수 변경

--- src/main/java/com/oasis/springboot/service/UserService.java
+++ src/main/java/com/oasis/springboot/service/UserService.java
@@ -1,6 +1,11 @@
 package com.oasis.springboot.service;
 
-import com.oasis.springboot.common.exception.*;
+import com.oasis.springboot.common.exception.EmptyAuthenticationException;
+import com.oasis.springboot.common.exception.ExistUserException;
+import com.oasis.springboot.common.exception.InvalidateUserException;
+import com.oasis.springboot.common.exception.NotMatchPasswordException;
+import com.oasis.springboot.common.handler.S3Uploader;
+import com.oasis.springboot.common.jwt.SecurityUtil;
 import com.oasis.springboot.domain.user.Role;
 import com.oasis.springboot.domain.user.User;
 import com.oasis.springboot.domain.user.UserRepository;
@@ -8,15 +13,11 @@ import com.oasis.springboot.dto.user.PasswordDto;
 import com.oasis.springboot.dto.user.SignUpRequestDto;
 import com.oasis.springboot.dto.user.UpdateUserRequestDto;
 import com.oasis.springboot.dto.user.UserMainResponseDto;
-import com.oasis.springboot.common.handler.S3Uploader;
-import com.oasis.springboot.common.jwt.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 
 @Service
@@ -28,12 +29,18 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final S3Uploader s3Uploader;
 
+    @Value("${user.default.image}")
+    private String defaultImage;
+
     @Transactional
-    public long signup(SignUpRequestDto signUpRequestDto){
+    public long signup(SignUpRequestDto signUpRequestDto) {
         String email = signUpRequestDto.getEmail();
         checkDuplicateEmail(email);
 
-        String fileUrl = s3Uploader.getFileS3Url(signUpRequestDto.getFile(), "static");
+        String fileUrl = defaultImage;
+        if (signUpRequestDto.getFile() != null) {
+            fileUrl = s3Uploader.getFileS3Url(signUpRequestDto.getFile(), "static");
+        }
 
         User user = User.builder()
                 .email(email)
@@ -52,32 +59,37 @@ public class UserService {
     public String updateUserInfo(UpdateUserRequestDto requestDto) {
         User user = findUser();
 
-        user.modifyNickName(requestDto.getNickName());
+        if (!requestDto.getNickName().equals(user.getNickName())) {
+            user.modifyNickName(requestDto.getNickName());
+        }
 
-        String fileUrl = s3Uploader.getFileS3Url(requestDto.getFile(), "static");
-
-        user.modifyPicture(fileUrl);
+        if (requestDto.getIsDefault().equals("true")) {
+            user.modifyPicture(defaultImage);
+        } else if (requestDto.getFile() != null) {
+            String fileUrl = s3Uploader.getFileS3Url(requestDto.getFile(), "static");
+            user.modifyPicture(fileUrl);
+        }
 
         return "success";
     }
 
     @Transactional
-    public String updatePassword(PasswordDto passwordDto){
+    public String updatePassword(PasswordDto passwordDto) {
         User user = findUser();
-        if(!passwordEncoder.matches(passwordDto.getCurrentPW(), user.getPassword()))
+        if (!passwordEncoder.matches(passwordDto.getCurrentPW(), user.getPassword()))
             throw new NotMatchPasswordException();
         String newPw = passwordEncoder.encode(passwordDto.getNewPW());
         user.modifyPassword(newPw);
         return "success";
     }
 
-    private void checkDuplicateEmail(String email){
-        if(userRepository.existsByEmail(email)){
+    private void checkDuplicateEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
             throw new ExistUserException();
         }
     }
 
-    private String findCurrentUserEmail(){
+    private String findCurrentUserEmail() {
         String email = SecurityUtil.getCurrentEmail()
                 .orElseThrow(EmptyAuthenticationException::new);
         return email;
@@ -91,18 +103,18 @@ public class UserService {
                 .orElseThrow(InvalidateUserException::new);
     }
 
-    public User findUser(){
+    public User findUser() {
         String email = findCurrentUserEmail();
         return userRepository.findByEmail(email).orElseThrow(InvalidateUserException::new);
 
     }
 
-    public User findByEmail(String email){
+    public User findByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(InvalidateUserException::new);
     }
 
 
-    public Long findUserId(){
+    public Long findUserId() {
         String email = findCurrentUserEmail();
         System.out.println(email);
         User user = userRepository.findByEmail(email).orElseThrow(InvalidateUserException::new);

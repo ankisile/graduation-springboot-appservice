8ba24da9d132026d0a23bae3605aac23cb585ef8
siss-ankisile <ankisile@gmail.com>
2023-06-26 19:57:56 +0900

    refactor: ExceptionController code formatting

--- src/main/java/com/oasis/springboot/controller/ExceptionController.java
+++ src/main/java/com/oasis/springboot/controller/ExceptionController.java
@@ -1,7 +1,6 @@
 package com.oasis.springboot.controller;
 
 import com.oasis.springboot.common.exception.*;
-import com.oasis.springboot.common.exception.Exception;
 import com.oasis.springboot.common.response.CommonResponse;
 import com.oasis.springboot.common.response.ResponseService;
 import lombok.RequiredArgsConstructor;
@@ -17,25 +16,25 @@ public class ExceptionController {
 
     @ExceptionHandler(InvalidateUserException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    private CommonResponse invalidateUserException(InvalidateUserException e){
-        return responseService.getErrorResponse(Exception.INVALIDATE_USER.getCode(), Exception.INVALIDATE_USER.getMessage());
+    private CommonResponse invalidateUserException(InvalidateUserException e) {
+        return responseService.getErrorResponse(ErrorCode.INVALIDATE_USER.getCode(), ErrorCode.INVALIDATE_USER.getMessage());
     }
 
     @ExceptionHandler(EmptyAuthenticationException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    private CommonResponse emptyAuthenticationException(EmptyAuthenticationException e){
-        return responseService.getErrorResponse(Exception.EMPTY_AUTHENTICATION.getCode(), Exception.EMPTY_AUTHENTICATION.getMessage());
+    private CommonResponse emptyAuthenticationException(EmptyAuthenticationException e) {
+        return responseService.getErrorResponse(ErrorCode.EMPTY_AUTHENTICATION.getCode(), ErrorCode.EMPTY_AUTHENTICATION.getMessage());
     }
 
     @ExceptionHandler(InvalidatePlantException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    private CommonResponse invalidatePlantException(InvalidatePlantException e){
-        return responseService.getErrorResponse(Exception.INVALIDATE_PLANT.getCode(), Exception.INVALIDATE_PLANT.getMessage());
+    private CommonResponse invalidatePlantException(InvalidatePlantException e) {
+        return responseService.getErrorResponse(ErrorCode.INVALIDATE_PLANT.getCode(), ErrorCode.INVALIDATE_PLANT.getMessage());
     }
 
     @ExceptionHandler(InvalidateGardenException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    private CommonResponse invalidateGardenException(InvalidateGardenException e){
-        return responseService.getErrorResponse(Exception.INVALIDATE_GARDEN.getCode(), Exception.INVALIDATE_GARDEN.getMessage());
+    private CommonResponse invalidateGardenException(InvalidateGardenException e) {
+        return responseService.getErrorResponse(ErrorCode.INVALIDATE_GARDEN.getCode(), ErrorCode.INVALIDATE_GARDEN.getMessage());
     }
 }

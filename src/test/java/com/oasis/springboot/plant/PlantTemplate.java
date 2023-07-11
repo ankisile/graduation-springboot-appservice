8bbd670b19aeeaf9a7b4f5d73e1c12abf515dd68
siss-ankisile <ankisile@gmail.com>
2023-07-11 22:19:12 +0900

    test: PlantService 테스트 코드 작성을 위한 PlantTemplate 작성 완료

--- src/test/java/com/oasis/springboot/plant/PlantTemplate.java
+++ src/test/java/com/oasis/springboot/plant/PlantTemplate.java
@@ -4,10 +4,12 @@ import com.oasis.springboot.dto.plant.PlantSaveRequestDto;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.FileInputStream;
+import java.time.LocalDate;
 
 public class PlantTemplate {
     public static final String NAME = "PLANTname";
     public static final String NAME2 = "PLANTname2";
+    public static final LocalDate ADOPTINGDATE = LocalDate.of(2023, 3, 3);
     public static final Integer WATERALARMINTERVAL = 2;
     public static final String WATERSUPPLY = "많음";
     public static final Double SUNSHINE = 3.0;
@@ -15,13 +17,16 @@ public class PlantTemplate {
     public static final Integer LOWTEMPERATURE = -10;
 
     public static PlantSaveRequestDto makePlantSaveRequestDtoWithFile() throws Exception {
-        return new PlantSaveRequestDto(NAME, null, WATERALARMINTERVAL, WATERSUPPLY, SUNSHINE, HIGHTEMPERATURE, LOWTEMPERATURE, getPlantImage());
+        return new PlantSaveRequestDto(NAME, null, ADOPTINGDATE, WATERALARMINTERVAL, WATERSUPPLY, SUNSHINE, HIGHTEMPERATURE, LOWTEMPERATURE, getPlantImage());
     }
 
     public static PlantSaveRequestDto makePlantSaveRequestDtoWithUrl() throws Exception {
-        return new PlantSaveRequestDto(NAME, "testImage.jpg", WATERALARMINTERVAL, WATERSUPPLY, SUNSHINE, HIGHTEMPERATURE, LOWTEMPERATURE, null);
+        return new PlantSaveRequestDto(NAME, "testImage.jpg", ADOPTINGDATE, WATERALARMINTERVAL, WATERSUPPLY, SUNSHINE, HIGHTEMPERATURE, LOWTEMPERATURE, null);
     }
 
+    public static PlantSaveRequestDto makeTestPlant2() throws Exception {
+        return new PlantSaveRequestDto(NAME2, null, ADOPTINGDATE, WATERALARMINTERVAL, WATERSUPPLY, SUNSHINE, HIGHTEMPERATURE, LOWTEMPERATURE, getPlantImage());
+    }
 
     public static MockMultipartFile getPlantImage() throws Exception {
         String fileName = "testImage";

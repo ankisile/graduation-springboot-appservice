48c6b57793e25f06103d3c19d08ce641f8b42adb
siss-ankisile <ankisile@gmail.com>
2023-07-30 22:54:42 +0900

    code: PlantController update - PlantController 리팩토링 및 코드 변경

--- src/main/java/com/oasis/springboot/controller/PlantController.java
+++ src/main/java/com/oasis/springboot/controller/PlantController.java
@@ -1,20 +1,20 @@
 package com.oasis.springboot.controller;
 
-import com.oasis.springboot.dto.plant.PlantDetailResponseDto;
-import com.oasis.springboot.dto.plant.PlantsResponseDto;
 import com.oasis.springboot.common.response.ListResponse;
 import com.oasis.springboot.common.response.ResponseService;
 import com.oasis.springboot.common.response.SingleResponse;
+import com.oasis.springboot.dto.plant.PlantDetailResponseDto;
+import com.oasis.springboot.dto.plant.PlantSaveRequestDto;
+import com.oasis.springboot.dto.plant.PlantsResponseDto;
 import com.oasis.springboot.service.PlantService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 
 @Tag(name = "Plant")
@@ -25,50 +25,33 @@ public class PlantController {
     private final PlantService plantService;
     private final ResponseService responseService;
 
-    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
-    @PostMapping(value = "",  consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public SingleResponse<String> savePlant(
-            @Schema(name = "PlantSaveRequestDto",
-                    description = "식물 저장(String이지만 json형식으로)",
-                    required = true,
-                    example = "{\n" +
-                            "  \"name\": \"string\",\n" +
-                            "  \"picture\": \"string\",\n" +
-                            "  \"waterAlarmInterval\": 7,\n" +
-                            "  \"waterSupply\": \"흙을 촉촉하게 유지함(물에 잠기지 않도록 주의)\",\n" +
-                            "  \"sunshine\": 5,\n" +
-                            "  \"highTemperature\": 23,\n" +
-                            "  \"lowTemperature\": 25\n" +
-                            "}"
-            )
-            @RequestPart("key") String requestDto,
-            @Parameter(name = "file", description = "사진(maxSize: 10MB)")
-            @RequestPart(value = "file") MultipartFile multipartFile ) {
-        return responseService.getSingleResponse(plantService.savePlant(requestDto, multipartFile));
+    @Operation(security = {@SecurityRequirement(name = "bearer-key")})
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public SingleResponse<Long> savePlant(@ModelAttribute @Valid PlantSaveRequestDto requestDto) {
+        return responseService.getSingleResponse(plantService.savePlant(requestDto));
     }
 
-    @Operation(summary = "get plants", description = "식물 조회 -> 홈화면, 식물일지 부분에서 쓰면 됨", security = { @SecurityRequirement(name = "bearer-key") })
+    @Operation(summary = "get plants", description = "식물 조회 -> 홈화면, 식물일지 부분에서 쓰면 됨", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping("")
-    public ListResponse<PlantsResponseDto> getPlants(){
+    public ListResponse<PlantsResponseDto> getPlants() {
         return responseService.getListResponse(plantService.getPlants());
     }
 
-    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
+    @Operation(security = {@SecurityRequirement(name = "bearer-key")})
     @DeleteMapping("/{plantId}")
     public SingleResponse<String> deletePlant(@PathVariable Long plantId) {
-
         return responseService.getSingleResponse(plantService.deletePlant(plantId));
     }
 
-    @Operation(description = "식물 디테일", security = { @SecurityRequirement(name = "bearer-key") })
+    @Operation(description = "식물 디테일", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping("/{plantId}")
-    public SingleResponse<PlantDetailResponseDto> getPlantDetail(@PathVariable Long plantId){
+    public SingleResponse<PlantDetailResponseDto> getPlantDetail(@PathVariable Long plantId) {
         return responseService.getSingleResponse(plantService.getPlantDetail(plantId));
     }
 
-    @Operation(summary = "대표 식물등록", security = { @SecurityRequirement(name = "bearer-key") })
+    @Operation(summary = "대표 식물등록", security = {@SecurityRequirement(name = "bearer-key")})
     @PatchMapping("/star/{plantId}")
-    public SingleResponse<String> makeStar(@PathVariable Long plantId){
+    public SingleResponse<String> makeStar(@PathVariable Long plantId) {
         return responseService.getSingleResponse(plantService.makeStar(plantId));
     }
 }

e39ac4f6287bd2dce7f0b6742dfdfb6710e6a402
siss-ankisile <ankisile@gmail.com>
2023-08-06 00:53:44 +0900

    refactor: GardenService code formatting

--- src/main/java/com/oasis/springboot/service/GardenService.java
+++ src/main/java/com/oasis/springboot/service/GardenService.java
@@ -1,38 +1,39 @@
 package com.oasis.springboot.service;
 
 import com.oasis.springboot.common.exception.InvalidateGardenException;
-import com.oasis.springboot.domain.bookmark.BookmarkRepository;
 import com.oasis.springboot.domain.garden.GardenRepository;
 import com.oasis.springboot.dto.garden.GardenDetailResponseDto;
 import com.oasis.springboot.dto.garden.GardenListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GardenService {
 
     private final GardenRepository gardenRepository;
     private final BookmarkService bookmarkService;
 
-    public List<GardenListResponseDto> getGardenList(){
+    public List<GardenListResponseDto> getGardenList() {
         return gardenRepository.findAll()
                 .stream()
                 .map(GardenListResponseDto::new)
                 .collect(Collectors.toList());
     }
 
-    public GardenDetailResponseDto getDetailGarden(Long id){
+    public GardenDetailResponseDto getDetailGarden(Long id) {
         boolean isBookmark = bookmarkService.isBookmark(id);
         return gardenRepository.findById(id)
                 .map(garden -> new GardenDetailResponseDto(garden, isBookmark))
                 .orElseThrow(InvalidateGardenException::new);
     }
 
-    public List<GardenListResponseDto> searchGardenList(String keyword){
+    public List<GardenListResponseDto> searchGardenList(String keyword) {
         return gardenRepository.findByNameContaining(keyword)
                 .stream()
                 .map(GardenListResponseDto::new)

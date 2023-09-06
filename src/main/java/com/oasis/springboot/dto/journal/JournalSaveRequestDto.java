997cb4d2ff4f4f335b437a4b60a4a7a74d23cd24
siss-ankisile <ankisile@gmail.com>
2023-09-07 01:44:08 +0900

    modify: JournalService의 saveJournal 함수 부분 변경

--- src/main/java/com/oasis/springboot/dto/journal/JournalSaveRequestDto.java
+++ src/main/java/com/oasis/springboot/dto/journal/JournalSaveRequestDto.java
@@ -6,20 +6,28 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class JournalSaveRequestDto {
+    @NotBlank(message = "내용이 없습니다.")
     private String content;
-    private String picture;
+    private MultipartFile file;
+    private String isFileExist;
 
     @Builder
-    public JournalSaveRequestDto(String content, String picture) {
+    public JournalSaveRequestDto(String content, MultipartFile file, String isFileExist) {
         this.content = content;
+        this.file = file;
+        this.isFileExist = isFileExist;
     }
 
-    public Journal toEntity(Plant plant){
+    public Journal toEntity(Plant plant, String picture) {
         return Journal.builder()
                 .content(content)
                 .picture(picture)
--- src/main/java/com/oasis/springboot/service/JournalService.java
+++ src/main/java/com/oasis/springboot/service/JournalService.java
@@ -1,25 +1,23 @@
 package com.oasis.springboot.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oasis.springboot.common.exception.InvalidatePlantException;
+import com.oasis.springboot.common.handler.S3Uploader;
 import com.oasis.springboot.domain.journal.Journal;
 import com.oasis.springboot.domain.journal.JournalRepository;
 import com.oasis.springboot.domain.plant.Plant;
 import com.oasis.springboot.domain.plant.PlantRepository;
 import com.oasis.springboot.dto.journal.JournalSaveRequestDto;
 import com.oasis.springboot.dto.journal.JournalsResponseDto;
-import com.oasis.springboot.common.handler.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class JournalService {
 
     private final JournalRepository journalRepository;
@@ -27,45 +25,35 @@ public class JournalService {
     private final S3Uploader s3Uploader;
 
     @Transactional
-    public String saveJournal(Long plantId, String dto, MultipartFile file){
-        ObjectMapper mapper = new ObjectMapper();
-        JournalSaveRequestDto requestDto = null;
-        try {
-            requestDto = mapper.readValue(dto, JournalSaveRequestDto.class);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+    public String saveJournal(Long plantId, JournalSaveRequestDto requestDto) {
 
-        try {
-            if(file!=null) {
-                String s3Url = s3Uploader.upload(file, "journal");
-                requestDto.setPicture(s3Url);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        String s3Url = null;
+        if (requestDto.getIsFileExist().equals("true")) {
+            s3Url = s3Uploader.getFileS3Url(requestDto.getFile(), "journal");
         }
 
         Plant plant = plantRepository.findById(plantId)
                 .orElseThrow(InvalidatePlantException::new);
 
-        Long journalId = journalRepository.save(requestDto.toEntity(plant)).getId();
+        Long journalId = journalRepository.save(requestDto.toEntity(plant, s3Url)).getId();
         Journal journal = journalRepository.findById(journalId)
-                .orElseThrow(()->new IllegalArgumentException("해당 일지가 없습니다. id = "+ journalId));
+                .orElseThrow(() -> new IllegalArgumentException("해당 일지가 없습니다. id = " + journalId));
         plant.updateRecentRecordDate(journal.getCreatedDate().toLocalDate());
         return "식물 일지 등록 성공";
     }
 
-    public List<JournalsResponseDto> getJournals(Long plantId){
+    public List<JournalsResponseDto> getJournals(Long plantId) {
         return journalRepository.findJournalsByPlantIdFetchJoin(plantId)
                 .stream()
                 .map(JournalsResponseDto::new)
                 .collect(Collectors.toList());
     }
 
-    public String deleteJournal(Long journalId){
+    @Transactional
+    public String deleteJournal(Long journalId) {
         Journal journal = journalRepository.findById(journalId)
-                .orElseThrow(()->new IllegalArgumentException("해당 일지가 없습니다. id = "+ journalId));
-        if(journal.getPicture() != null) {
+                .orElseThrow(() -> new IllegalArgumentException("해당 일지가 없습니다. id = " + journalId));
+        if (journal.getPicture() != null) {
             String str = journal.getPicture();
             String path = str.substring(62);
             s3Uploader.delete(path);
